287a2113c88eb75b3788067e1900606c06589385
Tayna Sousa <tayna.igt@hotmail.com>
2021-04-30 17:16:43 +0000

    Adds print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main: Entry point
+ *
+ * Return: Always 0
+ */
+
+/** prints alphabet in lowercase **/
+
+int main(){
+	char ch = 'a';
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar (ch);
+	return (0);
+}
