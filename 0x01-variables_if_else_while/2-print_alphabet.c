7b76107ae987b733244c8cd2b33130a0506dc4bf
Tayna Sousa <tayna.igt@hotmail.com>
2021-04-30 17:32:27 +0000

    Update documentation style

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,15 +6,13 @@
  * Return: Always 0
  */
 
-/** prints alphabet in lowercase **/
-
 int main(void)
 {
 	char ch = 'a';
 		
 	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar (ch);
-		putchar ('\n');
+		putchar(ch);
+		putchar('\n');
 
 	return (0);
 }
