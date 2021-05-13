5470ed5eb2929dc7f92be3819b28cf2a378405ad
Tayna Sousa <tayna.igt@hotmail.com>
2021-05-13 17:01:22 +0000

    Update numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,12 +11,10 @@
 int main(void)
 {
 
-	unsigned char num;
+	int num;
 
-	for(num = '0'; num <= '9'; num++)
-	{
-		putchar(num);
-	}
+	for(num = 0; num <= 9; num++)
+		putchar(( num % 10) + '0');	
 
 	putchar('\n');
 
