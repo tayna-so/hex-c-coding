3bc660005c095302b1779a1150faa8d988ba2fce
Tayna Sousa <tayna.igt@hotmail.com>
2021-05-13 17:05:05 +0000

    Update comb

--- 0x01-variables_if_else_while/8-print_comb.c
+++ 0x01-variables_if_else_while/8-print_comb.c
@@ -10,12 +10,12 @@
 int main(void)
 {
 
-	unsigned char num = '0';
+	int num = 0;
 
-	for(num = '0'; num <= '9'; num++)
+	for(num = 0; num <= 9; num++)
 	{
-		putchar(num);
-		if(num != '9')
+		putchar((num % 10) + 0);
+		if(num != 9)
 		{
 			putchar(',');
 			putchar(' ');
