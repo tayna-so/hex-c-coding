121bb49474e870f097c59fea4ea8a66b6744ea06
Tayna Sousa <tayna.igt@hotmail.com>
2021-06-07 16:14:06 +0000

    Updates betty coding style

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -7,10 +7,10 @@
 /**
  * add - adds two integers
  * @number1: The first number to be added
- * @number2: The second number to be added 
+ * @number2: The second number to be added
  * Return: The result of the addition
  */
 int add(int number1, int number2)
 {
-	return(number1 + number2);
+	return (number1 + number2);
 }
