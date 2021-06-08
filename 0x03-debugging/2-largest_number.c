e1df6f067446c4521ed22bbb9120c50e87394999
Tayna Sousa <tayna.igt@hotmail.com>
2021-06-08 03:06:08 +0000

    Updates Betty documentation style

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,10 +1,14 @@
+/*
+ * File: 2-largest_number.c
+ * Author: Tayna Sousa
+ */
 #include "holberton.h"
 
 /**
  * largest_number = returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
- * #c: third integer
+ * @c: third integer
  * Return: largest number
  */
 
