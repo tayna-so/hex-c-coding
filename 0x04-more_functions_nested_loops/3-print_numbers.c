91209f5d8ff8874d576fe7dcd820b99179aebb98
Tayna Sousa <tayna.igt@hotmail.com>
2021-07-23 01:15:34 +0000

    Updates Betty documentation style

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,11 @@
-/**
+/*
  * File: 3-print_numbers.c
  * Auth: Tayna Sousa
  */
 #include "holberton.h"
 
 /**
- * 3-print_numbers - prints the numbers, from 0 to 9
+ * print_numbers - Prints the numbers, from 0 to 9
  *
  * Return: Numbers 0 to 9 followed by a new line
  */
