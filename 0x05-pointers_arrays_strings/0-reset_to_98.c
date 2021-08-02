4bd3c15ff0c2569bf682b0fb113b4aad8257a5f0
Tayna Sousa <tayna.igt@hotmail.com>
2021-08-02 15:03:56 -0300

    Updates Kernel style code

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,15 @@
 /*
-*File: 0-reset_to_98.c
-*Auth: Tayna Sousa
-*/
+ * File: 0-reset_to_98.c
+ * Auth: Tayna Sousa
+ */
 
 #include "holberton.h"
 
 /**
  * reset_to_98 - it takes a poiter to an int
  * as parameter and updates the value it points to 98
+ * 
+ * @n: the pointer to an int
 **/
 
 void reset_to_98(int *n)
