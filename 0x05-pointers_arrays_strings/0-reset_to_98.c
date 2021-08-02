20e004a52d4b0647c5dd0adadc43c07b3060f805
Tayna Sousa <tayna.igt@hotmail.com>
2021-08-02 15:06:02 -0300

    Adds README.md file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -8,10 +8,8 @@
 /**
  * reset_to_98 - it takes a poiter to an int
  * as parameter and updates the value it points to 98
- * 
  * @n: the pointer to an int
-**/
-
+*/
 void reset_to_98(int *n)
 {
     *n = 98;
