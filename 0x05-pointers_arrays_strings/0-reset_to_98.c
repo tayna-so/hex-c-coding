129d08d94f48bc60d4424d8061ec1ce86376b68a
Tayna Sousa <tayna.igt@hotmail.com>
2021-08-02 14:46:02 -0300

    Adds files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+/*
+*File: 0-reset_to_98.c
+*Auth: Tayna Sousa
+*/
+
+#include "holberton.h"
+
+/**
+ * reset_to_98 - it takes a poiter to an int
+ * as parameter and updates the value it points to 98
+**/
+
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,24 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*
+ * File: holberton.h
+ * Auth: Tayna Sousa
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x05-pointers_arrays_strings directory
+ */
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+#endif /* HOLBERTON_H */
