7dda10668ded69d60fdfaa2f047d2a75dcb9ff84
Tayna Sousa <tayna.igt@hotmail.com>
2021-10-06 11:08:27 -0300

    Adds files

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+/** File: 0-strcat  Author: Tayna Sousa **/
+
+#include "holberton.h"
+
+/**
+ * _strcat - a function that concatenates two strings
+ * @dest: A pointer to the string
+ * @src: String to be appended
+ * Return: dest
+**/
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = j = 0;
+	while (dest[i++])
+		j++;
+
+	for (i = 0; src[i]; i++)
+		dest[j++] = src[i];
+
+	return (dest);
+}
