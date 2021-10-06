a0e6a2e5e00464028d93fd93be843549c27b228c
Tayna Sousa <tayna.igt@hotmail.com>
2021-10-06 11:16:34 -0300

    Adds files

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+/** File: 1-strncat.c  Author: Tayna Sousa **/
+
+#include "holberton.h"
+
+/**
+* _strncpy - it copies a string
+* @dest: A pointer to the string
+* @src: String to be copied
+* @n: concatenates caracters
+* Return: dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (*(src + j) != '\0' && n > j)
+	{
+		*(dest + j) = *(src + j);
+		j++;
+	}
+	while (n > j)
+	{
+		*(dest + j) = '\0';
+		j++;
+	}
+	return (dest);
+}
