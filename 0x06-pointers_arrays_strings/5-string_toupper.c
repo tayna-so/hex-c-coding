8ede80141cc389ebb8981b9cb32bcad9dec81f76
Tayna Sousa <tayna.igt@hotmail.com>
2021-10-07 11:00:19 -0300

    Adds files

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,13 +9,14 @@
 
 char *string_toupper(char *str)
 {
-int i = 0;
+	int i = 0;
 
-while (str[i])
-{
-if (str[i] >= 'a' && str[i] < 'z')
-str[i] -= 32;
-i++;
-}
-return (str);
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] < 'z')
+			str[i] -= 32;
+
+		i++;
+	}
+	return (str);
 }
