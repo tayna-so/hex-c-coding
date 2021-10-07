a4115edde7cb2bcc6c3da6d8e5b83307a1aea0ce
Tayna Sousa <tayna.igt@hotmail.com>
2021-10-07 11:04:10 -0300

    Updates Betty style

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+/** File: 6-cap_string.c  Auth: Tayna Sousa **/
+#include "holberton.h"
+
+/**
+ * cap_string - it capitalizes all words of a string
+ * @s: string
+ * Return: s
+ **/
+
+char *cap_string(char *s)
+{
+		int i = 0;
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= 32;
+	while (s[i] != '\0')
+	{
+		if ((s[i - 1] == ' ' || s[i - 1] == '\n'
+		|| s[i - 1] == '\t' || s[i - 1] == ','
+		|| s[i - 1] == '?' || s[i - 1] == '"'
+		|| s[i - 1] == ';' || s[i - 1] == '!'
+		|| s[i - 1] == '(' || s[i - 1] == ')'
+		|| s[i - 1] == '{' || s[i - 1] == '}'
+		|| s[i - 1] == '.')
+		&& (s[i] > 'a' && s[i] < 'z'))
+		{
+			s[i] -= 32;
+		}
+		else
+			s[i] = s[i];
+		i++;
+	}
+	return (s);
+}
