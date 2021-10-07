adf2c8b0464c9c6138d3a252499caf4213619047
Tayna Sousa <tayna.igt@hotmail.com>
2021-10-07 10:55:28 -0300

    Adds files

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+/** File 5-string_toupper.c  Auth: Tayna Sousa **/
+#include "holberton.h"
+
+/**
+ * string_toupper - it changes all lowercase letters of a string to uppercase
+ * @str: the string
+ * Return: a pointer to the string
+ **/
+
+char *string_toupper(char *str)
+{
+    int i = 0;
+
+    while (str[i])
+    {
+        if (str[i] >= 'a' && str[i] < 'z')
+            str[i] -= 32;
+
+        i++;
+    }
+    return (str);
+}
