16b4f8363479f24bcaace1731cc179d20a5b7ea1
Tayna Sousa <tayna.igt@hotmail.com>
2021-10-07 10:43:13 -0300

    Adds files

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+/** File: 4-rev_array.c  Auth: Tayna Sousa **/
+#include "holberton.h"
+
+/**
+ * reverse_array - it reverses the content of an array of integers
+ * @n: number of elements of the array
+ * @a: array
+ * Return: nothing
+**/
+
+void reverse_array(int *a, int n)
+{
+	int rev, i;
+
+    for (i = n - 1; i >= n / 2; i--)
+    {
+        rev = a[n - 1 - i];
+        a[n - 1 - i] = a[i];
+        a[i] = rev;
+    }
+}
